c99af809aafc291839f7665606573c02bfa2d443
Cera <tinh.phan@doitsolutions.vn>
2023-09-23 12:26:28 +0700

    add hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:hive/hive.dart';
 import 'package:your_track/app/app_widget.dart';
 
 import 'app/app_module.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Hive.initFlutter();
+
   return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
