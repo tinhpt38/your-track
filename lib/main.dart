4a402c14d38be84094b54c214f49a9707838df2c
Cera <tinh.phan@doitsolutions.vn>
2023-09-30 17:04:05 +0700

    initfire base

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:hive/hive.dart';
 import 'package:your_track/app/app_widget.dart';
-
 import 'app/app_module.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Hive.initFlutter();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   return runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
